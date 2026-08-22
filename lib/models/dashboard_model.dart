18d1e77126ee54a70720997e38b69dae72c9dd64
mostafanabih51-coder <mostafanabih51@gmail.com>
2026-08-22 07:21:37 +0300

    Update dashboard_model.dart

--- lib/models/dashboard_model.dart
+++ lib/models/dashboard_model.dart
@@ -1,55 +1,82 @@
 class DashboardModel {
   final int students;
+  final int activeStudents;
   final int teachers;
-  final int courses;
-  final int subscriptions;
-  final int attendance;
-  final List<DashboardActivity> activities;
+  final int todayClasses;
+  final int todayAttendance;
+  final double monthlyIncome;
+  final double monthlyExpenses;
+  final int expiring7Days;
 
   const DashboardModel({
     required this.students,
+    required this.activeStudents,
     required this.teachers,
-    required this.courses,
-    required this.subscriptions,
-    required this.attendance,
-    this.activities = const [],
+    required this.todayClasses,
+    required this.todayAttendance,
+    required this.monthlyIncome,
+    required this.monthlyExpenses,
+    required this.expiring7Days,
   });
 
-  factory DashboardModel.fromJson(Map<String, dynamic> json) {
-    final data = json['data'] is Map ? Map<String, dynamic>.from(json['data']) : json;
-    int number(dynamic value) => int.tryParse(value?.toString() ?? '') ?? 0;
-    final activityRaw = data['activities'] ?? data['recent_activities'] ?? [];
+  double get monthlyNet =>
+      monthlyIncome - monthlyExpenses;
+
+  factory DashboardModel.fromJson(
+    Map<String, dynamic> json,
+  ) {
+    final data = json['data'] is Map
+        ? Map<String, dynamic>.from(json['data'])
+        : json;
+
+    int number(dynamic value) {
+      return int.tryParse(
+            value?.toString() ?? '',
+          ) ??
+          0;
+    }
+
+    double money(dynamic value) {
+      return double.tryParse(
+            value?.toString() ?? '',
+          ) ??
+          0;
+    }
+
     return DashboardModel(
-      students: number(data['students'] ?? data['students_count'] ?? data['total_students']),
-      teachers: number(data['teachers'] ?? data['teachers_count'] ?? data['total_teachers']),
-      courses: number(data['courses'] ?? data['courses_count'] ?? data['total_courses']),
-      subscriptions: number(data['subscriptions'] ?? data['subscriptions_count'] ?? data['total_subscriptions']),
-      attendance: number(data['attendance'] ?? data['attendance_count'] ?? data['today_attendance']),
-      activities: activityRaw is List
-          ? activityRaw.whereType<Map>().map((e) => DashboardActivity.fromJson(Map<String, dynamic>.from(e))).toList()
-          : const [],
+      students: number(data['students']),
+      activeStudents: number(
+        data['active_students'],
+      ),
+      teachers: number(data['teachers']),
+      todayClasses: number(
+        data['today_classes'],
+      ),
+      todayAttendance: number(
+        data['today_attendance'],
+      ),
+      monthlyIncome: money(
+        data['monthly_income'],
+      ),
+      monthlyExpenses: money(
+        data['monthly_expenses'],
+      ),
+      expiring7Days: number(
+        data['expiring_7_days'],
+      ),
     );
   }
 
-  factory DashboardModel.empty() => const DashboardModel(
-        students: 0,
-        teachers: 0,
-        courses: 0,
-        subscriptions: 0,
-        attendance: 0,
-      );
-}
-
-class DashboardActivity {
-  final String title;
-  final String subtitle;
-  final String time;
-
-  const DashboardActivity({required this.title, required this.subtitle, required this.time});
-
-  factory DashboardActivity.fromJson(Map<String, dynamic> json) => DashboardActivity(
-        title: (json['title'] ?? json['name'] ?? '').toString(),
-        subtitle: (json['subtitle'] ?? json['description'] ?? '').toString(),
-        time: (json['time'] ?? json['created_at'] ?? '').toString(),
-      );
+  factory DashboardModel.empty() {
+    return const DashboardModel(
+      students: 0,
+      activeStudents: 0,
+      teachers: 0,
+      todayClasses: 0,
+      todayAttendance: 0,
+      monthlyIncome: 0,
+      monthlyExpenses: 0,
+      expiring7Days: 0,
+    );
+  }
 }
